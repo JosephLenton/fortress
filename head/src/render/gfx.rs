dedabde09c86a6c1ae74c143704c17d7de542029
joseph.lenton@askattest.com <joseph.lenton@askattest.com>
2018-01-29 04:23:51 +0000

    moved documentation from impl to trait

--- head/src/render/gfx.rs
+++ head/src/render/gfx.rs
@@ -6,51 +6,41 @@ use to_sdl2::*;
 
 use util::shapes::Rect;
 
-///
 /// Represents the graphics state.
 ///
 /// This handles all drawing logic.
-///
 pub trait GFX {
+    /// Call this before you begin drawing.
     fn clear(&mut self) -> ();
 
+    /// Draws a rectangle at the location given,
+    /// with the given colour.
     fn rectangle(&mut self, colour: Colour, rectable: Rect<f32>) -> ();
 
+    /// Draws a rectangle at the location given,
+    /// with the given colour.
     fn rectangle_outline(&mut self, colour: Colour, rectable: Rect<f32>) -> ();
 
+    /// Call when all the drawing is over for the current loop.
     fn finished_drawing(&mut self) -> ();
 }
 
 impl GFX for WindowCanvas {
-    ///
-    /// Call this before you begin drawing.
-    ///
     fn clear(&mut self) {
         self.set_draw_color(colour::BLACK.to_sdl2());
         self.clear();
     }
 
-    ///
-    /// Draws a rectangle at the location given,
-    /// with the given colour.
-    ///
     fn rectangle(&mut self, colour: Colour, rect: Rect<f32>) {
         self.set_draw_color(colour.to_sdl2());
         self.fill_rect(rect.to_sdl2());
     }
 
-    ///
-    /// Draws a rectangle at the location given,
-    /// with the given colour.
-    ///
     fn rectangle_outline(&mut self, colour: Colour, rect: Rect<f32>) {
         self.set_draw_color(colour.to_sdl2());
         self.draw_rect(rect.to_sdl2());
     }
 
-    ///
-    /// Call when all the drawing is over for the current loop.
-    ///
     fn finished_drawing(&mut self) {
         self.present();
     }
