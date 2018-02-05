2c284a4270aaf514d445d66658c69f91e5ec5a14
joseph.lenton@askattest.com <joseph.lenton@askattest.com>
2018-02-05 00:37:16 +0000

    moved a lot of the printing and colour code out of world

--- world/src/lib.rs
+++ world/src/lib.rs
@@ -5,12 +5,24 @@
 //!
 extern crate util;
 
+/// How time and dates work in the world.
 pub mod calendar;
+
+/// How weather works in the world.
 pub mod weather;
+
+/// The player management in the world.
 pub mod player;
+
+/// The world's tiles.
 pub mod tiles;
+
+/// Worlds maps.
 pub mod map;
-pub mod load;
-pub mod print;
-pub mod colour;
+
+/// How to describe a new world.
 pub mod world_setup;
+
+/// How to load things in the world.
+#[deprecated(note="needs to be moved out of fortress/world")]
+pub mod load;
--- world/src/tiles/mod.rs
+++ world/src/tiles/mod.rs
@@ -1,5 +1,5 @@
-pub mod tile_colour;
 pub mod display_tile;
 
 mod tile;
 pub use self::tile::Tile;
+
